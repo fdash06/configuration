9a4354e06815a49988c6c009e3111ce8dca27b67
fdash <sfukuda.fdash@gmail.com>
2015-09-10 00:15:58 +0900

    Modify setup script.

--- setup.sh
+++ setup.sh
@@ -3,9 +3,11 @@
 cd ~
 rm .vimrc
 rm .zshrc
+rm .tigrc
 \rm -rf .vim
 \rm -rf .zsh
 ln -s configuration/.vimrc
 ln -s configuration/.zshrc
 ln -s configuration/.vim
 ln -s configuration/.zsh
+ln -s configuration/.tigrc
